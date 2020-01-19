ac21830f632c7d7eb415878d1a76a77e65760926
Santosh Subramanya <santosh.subramanya@sympli.com.au>
2020-01-20 09:05:48 +1100

    commit web verify

--- test/api_test/specs/web-verify-test.js
+++ test/api_test/specs/web-verify-test.js
@@ -1,21 +1,17 @@
 const fs = require("fs");
 const readline = require("readline");
-const lineReader = require('line-reader');
+const lineReader = require("line-reader");
 
 describe("sbs radio location test", () => {
   it("should get hindi radio list", () => {
     browser.navigateTo("https://www.sbs.com.au/language/hindi/program");
 
-   
     let rows = $$(".audio-playlist__body .audio-playlist__item");
     let rowCount = rows.length;
     console.log("row count is", rowCount);
 
-    
-
-lineReader.eachLine('Output.txt', function(line) {
-    console.log(line);
-});
-    
+    lineReader.eachLine("Output.txt", function(line) {
+      console.log(line);
+    });
   });
 });
